8847f2713ab68f9adf826b33ef8fad9f0e398d4f
Zaffino <46814695+Zaffino@users.noreply.github.com>
2020-01-07 13:04:45 +0100

    Updates

---
+++ precache-manifest.d0eabb94714d3e895bd585d2f249552a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b174bdee842c8dfeb28e630036aca8e6",
+    "url": "/lavoroNatale/index.html"
+  },
+  {
+    "revision": "4f1f3c91efc8d34f8bcf",
+    "url": "/lavoroNatale/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fc585eaafad075eda566",
+    "url": "/lavoroNatale/static/js/2.4904d861.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/lavoroNatale/static/js/2.4904d861.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f1f3c91efc8d34f8bcf",
+    "url": "/lavoroNatale/static/js/main.47ddec78.chunk.js"
+  },
+  {
+    "revision": "3753c87487f2f1bb77a4",
+    "url": "/lavoroNatale/static/js/runtime-main.b07675b2.js"
+  }
+]);
